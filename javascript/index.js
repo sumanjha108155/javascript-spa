9e66ac2d066dffac9793b054fe41d0cc4cddf6c4
suman jha <suman.j@simformsolutions.com>
2023-01-25 18:20:03 +0530

    added spa files

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+function home() {
+    window.location.href = '#home';
+}
+
+function about() {
+    window.location.href = '#aboutus';
+}
+
+function image() {
+    window.location.href = '#images';
+}
+
+function services() {
+    window.location.href = '#services';
+}
+
